fb83940166e17a5e7af5d84027fa9c9fd8f1ee88
Michael O <michael-ortega>
2017-04-13 23:33:55 -0700

    added commentation

--- src/appserver/job/impl/Fibonacci.java
+++ src/appserver/job/impl/Fibonacci.java
@@ -3,13 +3,14 @@ package appserver.job.impl;
 import appserver.job.Tool;
 
 public class Fibonacci implements Tool{
-
+    //create a new instance of the helper class to calculate the fibonacci sequence
     FibonacciAux helper = null;
     
     @Override
     public Object go(Object parameters) {
-        
+        //call the helper class and pass in the parameters storing the result in the helper variable
         helper = new FibonacciAux((Integer) parameters);
+	//then simply return the final result back
         return helper.getResult();
     }
 }
