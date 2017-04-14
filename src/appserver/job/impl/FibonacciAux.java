0f742c5390ad8842e3b09d29589f006d4e3c81e4
Michael O <michael-ortega>
2017-04-13 23:35:22 -0700

    added commentation

--- src/appserver/job/impl/FibonacciAux.java
+++ src/appserver/job/impl/FibonacciAux.java
@@ -1,9 +1,9 @@
 package appserver.job.impl;
-
+//this class will actually compute the fibonacci result
 public class FibonacciAux {
-    
+    //instantiate a number to store the final result
     Integer number = null;
-    
+    //set this number to be the number passed in to the class
     public FibonacciAux(Integer number) {
         this.number = number;
     }
@@ -11,7 +11,7 @@ public class FibonacciAux {
     public Integer getResult() {
         return new Integer(fibonacci(number));
     }
-
+    //a simple and efficient recursive function for getting any fibonacci number
     public int fibonacci(int n)  {
     if(n == 0)
         return 0;
